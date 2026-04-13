b6e052fbefab1b441573cf0de81a7dfcd59230ad
HansMosesP <mosesdrivefto@gmail.com>
2026-04-13 19:56:21 +0700

    update

--- main.js
+++ main.js
@@ -1,28 +1,29 @@
 const express = require('express');
-const mongoose = require('mongoose');
 require('dotenv').config();
+const connectDB = require('./utils/database');
+const registerRoute = require('./api/register');
+const loginRoute = require('./api/login');
+const profileRoute = require('./api/profile');
+const musicRoute = require('./api/music');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log("✅ MongoDB Connected"))
-    .catch(err => console.error("❌ MongoDB Error:", err));
+connectDB();
 
-const laguSchema = new mongoose.Schema({
-    nama: String,
-    artist: String,
-    file: String
-});
-
-const Lagu = mongoose.model("Lagu", laguSchema);
-
+app.use(express.json());
 app.use(express.static('public'));
 
+// Routes
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
+app.use('/api/auth/register', registerRoute);
+app.use('/api/auth/login', loginRoute);
+app.use('/api/profile/', profileRoute);
+// app.use('/api/history/', historyRoute);
+app.use('/api/music/', musicRoute);
 
 app.listen(port, () => {
     console.log(`🚀 Server running on http://localhost:${port}`);
