82f279b9b18c685306083ffbfe4ef1e64128f6be
Andreas Setiawan Harefa <andreassh09@gmail.com>
2026-04-19 16:32:00 +0700

    upadate dari search,discovery,search history <Andreas>

---
+++ Music-Player-andreas/models/SearchHistory.js
@@ -0,0 +1,20 @@
+// andreas 535230105 
+const mongoose = require("mongoose");
+
+const searchHistorySchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  keyword: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("SearchHistory", searchHistorySchema);
